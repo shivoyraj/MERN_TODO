d76280dafd9f7828a66c9b93507a169201517b44
shivoyraj <shivoyraj78@gmail.com>
2024-02-10 23:28:07 +0530

    backend ready for deployment

--- backend/configs/mongoose.js
+++ backend/configs/mongoose.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-const MONGODB_URI = 'mongodb://127.0.0.1/todoapp';
+const MONGODB_URI = 'mongodb+srv://admin:admin@shioyraj.ybxncvi.mongodb.net/todoapp';
+//const MONGODB_URI = 'mongodb://127.0.0.1/todoapp';
 async function connectToDatabase() {
     try {
         await mongoose.connect(MONGODB_URI);
